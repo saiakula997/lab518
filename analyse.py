e6396155586e1fdd185ea4497e52a3c5cf1cb6c1
Just4Share <justforshare7@gmail.com>
2023-04-07 23:44:18 -0500

    Preethi hand top and bottom readings comparision

--- analyse.py
+++ analyse.py
@@ -9,25 +9,30 @@ def get_averages(file_name):
     return (top.mean(), bottom.mean())
 
 def get_readings_subjects(subjects_dir_path):
-    readings = {}
+    top_readings = {}
+    bottom_readings = {}
     for name in os.listdir(subjects_dir_path):
         path = PATH + "/" + name
-        readings[name] = []
+        top_readings[name] = []
+        bottom_readings[name] = []
         for file in os.listdir(path):
             top, bottom = get_averages(path+'/'+file)
-            readings[name].append(top)
-            readings[name].append(bottom)
-    return readings
+            top_readings[name].append(top)
+            bottom_readings[name].append(bottom)
+            print(file, top, bottom)
+    return top_readings, bottom_readings
 
 
-def plot_readings(readings):
+def plot_readings(readings, title):
     for name in readings.keys():
-        print(name, readings[name])
         plt.scatter( range(len(readings[name])), (readings[name]) , label=name)
     plt.legend()
+    plt.title(title)
+    plt.savefig(title + ".png")
     plt.show()
 
 
 PATH = "./observations/subjects"
-readings = get_readings_subjects(PATH)
-plot_readings(readings)
+top_readings, bottom_readings = get_readings_subjects(PATH)
+plot_readings(top_readings, "Top")
+plot_readings(bottom_readings, "Bottom")
