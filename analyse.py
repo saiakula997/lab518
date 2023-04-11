f9b6e84d47fa71948742b97e8d93ab1078fa211e
Just4Share <justforshare7@gmail.com>
2023-04-10 23:54:32 -0500

    Added figures

--- analyse.py
+++ analyse.py
@@ -1,7 +1,10 @@
 
 import os
+import argparse
 import pandas as pd
 import  matplotlib.pyplot as plt
+
+
 
 def get_averages(file_name):
     df = pd.read_csv(file_name)
@@ -12,14 +15,15 @@ def get_readings_subjects(subjects_dir_path):
     top_readings = {}
     bottom_readings = {}
     for name in os.listdir(subjects_dir_path):
-        path = PATH + "/" + name
+        path = subjects_dir_path + "/" + name
         top_readings[name] = []
         bottom_readings[name] = []
-        for file in os.listdir(path):
-            top, bottom = get_averages(path+'/'+file)
-            top_readings[name].append(top)
-            bottom_readings[name].append(bottom)
-            print(file, top, bottom)
+        if os.path.isdir(path):    
+            for file in os.listdir(path):
+                top, bottom = get_averages(path+'/'+file)
+                top_readings[name].append(top)
+                bottom_readings[name].append(bottom)
+                print(file, top, bottom)
     return top_readings, bottom_readings
 
 markers = ["." , "," , "o" , "v" , "^" , "<", ">"]
@@ -31,9 +35,13 @@ def plot_readings(top_readings, bottom_readings, title):
         person += 1
     plt.legend()
     plt.title(title)
+    plt.savefig(args.folder+"/"+"figure.png")
     plt.show()
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--folder', type=str, required=True)
+args = parser.parse_args()
 
-PATH = "./observations/subjects/subjects"
-top_readings, bottom_readings = get_readings_subjects(PATH)
+
+top_readings, bottom_readings = get_readings_subjects(args.folder)
 plot_readings(top_readings, bottom_readings, "PLOT")
