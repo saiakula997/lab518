f67766e31cdd0688fc1e15b03f6e4dae6ce56b79
Just4Share <justforshare7@gmail.com>
2023-04-17 13:23:00 -0500

    Modified Analyse Script

--- analyse.py
+++ analyse.py
@@ -7,16 +7,17 @@ import  matplotlib.pyplot as plt
 def get_averages(file_name):
     df = pd.read_csv(file_name)
     ch1, ch2, ch3, ch4 = df['ch1'], df['ch2'], df['ch3'], df['ch4']
-    return ( ((ch1.mean()+ch3.mean())/2),  ((ch2.mean()+ch4.mean())/2))
+    print(file_name, ch1.mean(), ch2.mean(), ch3.mean(), ch4.mean())
+    return ( ((ch1.mean()+ch2.mean())/2),  ((ch3.mean()+ch4.mean())/2))
 
 def get_readings_subjects(subjects_dir_path):
     top_readings = {}
     bottom_readings = {}
     for name in os.listdir(subjects_dir_path):
         path = subjects_dir_path + "/" + name
-        top_readings[name] = []
-        bottom_readings[name] = []
-        if os.path.isdir(path):    
+        if os.path.isdir(path):
+            top_readings[name] = []
+            bottom_readings[name] = []     
             for file in os.listdir(path):
                 top, bottom = get_averages(path+'/'+file)
                 top_readings[name].append(top)
