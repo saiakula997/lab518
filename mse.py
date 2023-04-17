2508ef7237d320810d3701a8d12db306d3150c8f
Just4Share <justforshare7@gmail.com>
2023-04-17 13:21:23 -0500

    Mean Square Error Calculation

--- mse.py
+++ mse.py
@@ -1,7 +1,6 @@
 import csv
 import argparse
 import pandas as pd
-from sklearn.metrics import mean_squared_error
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file1', type=str, required=True)
@@ -12,16 +11,8 @@ one = pd.read_csv(args.file1)
 two = pd.read_csv(args.file2)
 
 total_mse = 0
-cols = min(len(one), len(two))
 channels = ['ch1','ch2', 'ch3','ch4', 'ch5','ch6', 'ch7','ch8']
 for ch in channels:
-    mse = mean_squared_error(one[ch][:cols], two[ch][:cols])
-    print("Mean Square Error - ", ch, mse)
+    mse = (one[ch].mean() - two[ch].mean())**2
     total_mse += mse
 print("Total Mean Square Error", total_mse/len(channels))
-
-
-    
-
-
-
