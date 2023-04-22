85a75b2076dacf5a417aac60beb5c680827e9412
Just4Share <justforshare7@gmail.com>
2023-04-22 10:48:11 -0500

    Added Menu to flasher

--- mIPU/flasher.py
+++ mIPU/flasher.py
@@ -24,7 +24,7 @@ def address_split(address): return [ (address >> 16) & 0xFF, (address >> 8) & 0x
 
 # Read Commands
 def CMD_MAIN_MEM_PG_READ(address): return [0xD2] + address_split(address) + DUMMY_BITS_32 # cmd + address (to read a page)
-def CMD_CONT_ARRAY_READ_LF(address): return [0xD2] + address_split(address) + DUMMY_BITS_32 # cmd + address (to read a page)
+def CMD_CONT_ARRAY_READ_LF(address): return [0x03] + address_split(address)  # cmd + address 
 
 # Write Commands
 
@@ -45,14 +45,13 @@ def Get_Device_ID():
     M_ID, D_ID = response[1], (response[2]<<8 | response[3]) # ignoring response[4,5] EDI Data  
     return M_ID, D_ID
 
+def Read_n_Bytes_Address(address, n):
+    data = [ 0x00, ] * n
+    response = spi.xfer2(CMD_CONT_ARRAY_READ_LF(address) + data)
+    return response[-n:]
 
-
-# Set up SPI interface
-spi = spidev.SpiDev()
-spi.open(0, 0)  # SPI bus 0, device 0
-spi.max_speed_hz = 5000000  # Set SPI clock speed
-spi.mode = 0b11
-
+def Write_n_Bytes_Address(address, data):
+    pass
 
 def read_status(txt):
     # Send command to read status register
@@ -61,11 +60,56 @@ def read_status(txt):
     print(txt, end='')
     print(" --> Status Register", [hex(x) for x in response] )
 
+def print_menu():
+    print("################################################################")
+    print("1. Get Status Register")
+    print("2. Write 'n' Bytes into Address")
+    print("3. Read 'n' Bytes from Address")
+    print("4. Write into multiple pages")
+    print("5. Read from multiple pages")
+    print("6. Get Device ID(s)")
+    print("q. quit")
+    print("################################################################")
+    print("Enter Command Choice : ")
+    choice = input()
+    return choice
 
+def execute_cmd(choice):
+    if choice == '1':
+        Print_Status_Register()
+    elif choice == '2':
+        pass
+    elif choice == '6':
+        print(Get_Device_ID())
+
+
+# Set up SPI interface
+spi = spidev.SpiDev()
+spi.open(0, 0)  # SPI bus 0, device 0
+spi.max_speed_hz = 5000000  # Set SPI clock speed
+spi.mode = 0b11
+
+# setup GPIO 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(8, GPIO.OUT)
 
-# Send command to read device ID
+if __name__ == "__main__" :
+
+    while True:
+        choice = print_menu()
+        execute_cmd(choice)
+        
+        if choice == 'q':
+            break
+
+    # exit routine
+    GPIO.cleanup()
+    spi.close()
+
+
+
+
+    # Send command to read device ID
 command = [0x9F]  # Device ID read command
 response = spi.xfer2(command + [0x00, 0x00, 0x00, 0x00, 0x00])  # Send command and receive 4 bytes of response
 print("raw device ID : ", ([hex(x) for x in response]))
@@ -94,6 +138,4 @@ read_status("After Reading ")
 #read_data = response[4:]  # Extract data bytes from response
 #print("Read Data: ", read_data)
 
-GPIO.cleanup()
-# Close SPI interface
-spi.close()
+
