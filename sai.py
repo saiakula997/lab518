a307182865ced7aa72f193251e84e1a1c8ed3be5
saiakula997 <saiakula997@gmail.com>
2023-05-03 20:08:30 -0500

    collected data

--- sai.py
+++ sai.py
@@ -4,10 +4,11 @@ import argparse
 import Adafruit_ADS1x15
 
 GAIN = (2/3)
-TIME_S = (30)
+TIME_S = 480
 CHANNEL_COUNT = 4
 SAMPLE_FREQUENCY = (128)
-CSV_FILE_NAME = "test.csv"
+CSV_FILE_NAME = "Preethi_sample_2.csv"
+
 
 pga_fsv = { 
     2/3 : 6.144,
