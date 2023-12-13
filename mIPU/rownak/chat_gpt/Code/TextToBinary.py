dfbbc1b8e11e3e573c66dd2486b496d6d0f218eb
saiakula997 <saiakula997@gmail.com>
2023-12-13 13:40:28 -1000

    refactoring code

--- mIPU/rownak/chat_gpt/Code/TextToBinary.py
+++
@@ -1,8 +0,0 @@
-def save_text_to_binary_file(text, file_path):
-    with open(file_path, 'wb') as file:
-        file.write(text.encode('utf-8'))
-
-print("Please enter your query:")
-Text_query = input()
-binary_file_path = r"/home/lab518/DATA/Query.bin"
-save_text_to_binary_file(Text_query, binary_file_path)
