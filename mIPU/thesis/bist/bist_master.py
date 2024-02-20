a81f11408378264be406e2e8eb0057116610a673
Just4Share <justforshare7@gmail.com>
2024-02-19 19:42:16 -0600

    Added menu to bist master

--- mIPU/thesis/bist/bist_master.py
+++ mIPU/thesis/bist/bist_master.py
@@ -15,9 +15,82 @@ def gen_random_string(length=16):
 
 def callback_from_slave_1(channel):
     print("callback_from_slave_1")
+    print('Bus controller select Master')
+    bus_controller.select_master()
+    print('Read from external memory at : ', prj_conf['SLAVE_1_OUTPUT_ADDR'])
+    ext_mem.Read_String_n_Bytes_Address(prj_conf['SLAVE_1_OUTPUT_ADDR'], 16)
 
 def callback_from_slave_2(channel):
     print("callback_from_slave_2")
+    print('Bus controller select Master')
+    bus_controller.select_master()
+    print('Read from external memory at : ', prj_conf['SLAVE_2_OUTPUT_ADDR'])
+    ext_mem.Read_String_n_Bytes_Address(prj_conf['SLAVE_2_OUTPUT_ADDR'], 16)
+
+def print_menu():
+    print('##############################################')
+    print('1. Signal Slave-1')
+    print('2. Signal Slave-2')
+    print('3. Select Slave-1')
+    print('4. Select Slave-2')
+    print('5. Enter Input to Slave-1')
+    print('6. Enter Input to Slave-2')
+    print('7. Output from Slave-1')
+    print('8. Output from Slave-2')
+    print('##############################################')
+    return input("Enter your choice : ")
+
+def process(choice):
+    if(choice == 1):
+        gpio.signal_slave_1()
+    elif(choice == 2):
+        gpio.signal_slave_2()
+    elif(choice == 3):
+        bus_controller.select_slave_1()
+    elif(choice == 4):
+        bus_controller.select_slave_2()
+    elif(choice == 5):
+        print('5. Enter Input to Slave-1')
+        print('Bus controller select Master')
+        bus_controller.select_master()
+
+        print("Write text into external memory at : ", prj_conf['SLAVE_1_INPUT_ADDR'])
+        time.sleep(1)
+        
+        print('Bus controller select Slave-1')
+        bus_controller.select_slave_1()
+
+        print('Signal Slave-1')
+        gpio.signal_slave_1()
+
+        print('Wait for Ack from slave-1')
+    elif(choice == 6):
+        print('6. Enter Input to Slave-2')
+        print('Bus controller select Master')
+        bus_controller.select_master()
+
+        print("Write text into external memory at : ", prj_conf['SLAVE_2_INPUT_ADDR'])
+        time.sleep(1)
+        
+        print('Bus controller select Slave-2')
+        bus_controller.select_slave_2()
+
+        print('Signal Slave-2')
+        gpio.signal_slave_2()
+
+        print('Wait for Ack from slave-2')
+    elif(choice == 7):
+        print('7. Output from Slave-1')
+        print('Bus controller select Master')
+        bus_controller.select_master()
+        print('Read from external memory at : ', prj_conf['SLAVE_1_OUTPUT_ADDR'])
+        ext_mem.Read_String_n_Bytes_Address(prj_conf['SLAVE_1_OUTPUT_ADDR'], 16)
+    elif(choice == 8):
+        print('8. Output from Slave-2')
+        print('Bus controller select Master')
+        bus_controller.select_master()
+        print('Read from external memory at : ', prj_conf['SLAVE_2_OUTPUT_ADDR'])
+        ext_mem.Read_String_n_Bytes_Address(prj_conf['SLAVE_2_OUTPUT_ADDR'], 16)
 
 if __name__ == "__main__":
     gpio.init("MASTER")
@@ -58,6 +131,10 @@ if __name__ == "__main__":
     bus_controller.select_slave_1()
     gpio.signal_slave_1()
     time.sleep(30)
+
+    while(True):
+        choice = print_menu
+        process(choice)
 
     gpio.deinit()
     ext_mem.deinit()
